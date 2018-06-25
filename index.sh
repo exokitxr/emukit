9476e287c7af8cdff26672453284eee1d27e8faa
Avaer Kazmer <a@modules.io>
2018-06-25 00:48:42 -0400

    Update indexer script

--- index.sh
+++ index.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-./indexer ./assets/frontend > ./assets/frontend/.index-xhr
+./indexer ./assets/frontend > ./assets/frontend/bundle/.index-xhr
 ./indexer ./assets/cores > ./assets/cores/.index-xhr
